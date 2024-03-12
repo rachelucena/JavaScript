a26480de5d7c9eccf26fea4f5f497aa176a8ffab
rachelucena <159203793+rachelucena@users.noreply.github.com>
2024-03-12 06:12:30 -0400

    Exercicios modulo D curso de JavaScript

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora sao ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // Bom dia!
+        img.src = '../fotomanha.png'
+        document.body.style.background = '#fee776'
+    } else if (hora >= 12 && hora < 18) {
+        // Boa tarde!
+        img.src = '../fototarde.png'
+        document.body.style.background = '#feb813'
+    } else {
+        img.src = '../fotonoite.png'
+        document.body.style.background = '#232c36'
+        // Boa noite!
+    }
+}
