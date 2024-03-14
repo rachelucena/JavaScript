8397c853615c8eed5d725fa7344557032c2795cb
rachelucena <159203793+rachelucena@users.noreply.github.com>
2024-03-14 14:17:26 -0400

    Modulo F - Variaveis compostas

---
+++ aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [5, 8, 1, 9, 3]
+console.log(`Nosso vetor e ${num}`)
+
+let pos = num.indexOf(9)
+if [pos == -1] {
+    console.log(`O valor no foi encontrado`)
+} else {
+    console.log(`O valor esta na posicao ${pos}`)
+}
